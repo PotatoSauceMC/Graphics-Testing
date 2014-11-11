94d00da6473a2e3a52658e2c768fe312d150b5ff
Rj <heelyskidrj@gmail.com>
2014-11-10 23:31:24 -0500

    Create Boxy.java

---
+++ Boxy.java
@@ -0,0 +1,49 @@
+package tc.cornhub.psmc;
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Boxy {
+	static int x;
+	static int y;
+	int size = 10;
+	int speedMod = 2;
+	
+	
+	static boolean goingLeft = false;
+	static boolean goingRight = false;
+	static boolean goingUp = false;
+	static boolean goingDown = false;
+	
+	@SuppressWarnings("static-access")
+	public Boxy(int x, int y){
+		this.x = x * speedMod;
+		this.y = y * speedMod;
+		
+	}
+	
+	public void tick(Main main) {
+		
+		
+		if(goingUp && y > 0){
+			y--;
+		}
+		if(goingDown && y < Main.HEIGHT){
+			y++;
+		}
+		if(goingRight && x < Main.WIDTH) {
+			x++;
+		}
+		if(goingLeft && x > 0){
+			x--;
+			
+		}
+		
+		
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(Main.fx, Main.fy, size, size);
+	}
+}
